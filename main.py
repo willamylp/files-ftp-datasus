c7ede14b80c159bb1bc96a1876a20b1848193684
WILLAMY DOMINGOS <2015096932@emcm.caico>
2021-09-29 16:27:06 -0300

    Add Download Files CNES

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class Download_FTP:
         self.ui = Ui_Sys_Datasus()
         self.ui.setupUi(self.main_win)
 
+        self.ui.checkCNES.clicked.connect(self.checked)
         self.ui.checkBPA.clicked.connect(self.checked)
         self.ui.checkSISAIH01.clicked.connect(self.checked)
         self.ui.checkSIHD2.clicked.connect(self.checked)
@@ -21,9 +22,15 @@ class Download_FTP:
         self.main_win.show()
 
     def checked(self):
+        self.chkCNES = self.ui.checkCNES.isChecked()
         self.chkBPA = self.ui.checkBPA.isChecked()
         self.chkSISAIH01 = self.ui.checkSISAIH01.isChecked()
         self.chkSIHD2 = self.ui.checkSIHD2.isChecked()
+
+        if(self.chkCNES):
+            self.ui.input_CMPT_CNES.setEnabled(True)
+        else:
+            self.ui.input_CMPT_CNES.setEnabled(False)
 
         if(self.chkBPA):
             self.ui.input_BPA.setEnabled(True)
@@ -42,17 +49,24 @@ class Download_FTP:
             self.ui.input_CMPT.setEnabled(False)
             self.ui.input_VERSAO.setEnabled(False)
         
-        if(self.chkBPA or self.chkSIHD2 or self.chkSISAIH01):
+        if(self.chkCNES or self.chkBPA or self.chkSIHD2 or self.chkSISAIH01):
             self.ui.DownloadButton.setEnabled(True)
-            
-        elif(self.chkBPA and self.chkSIHD2 and self.chkSISAIH01):
+        else:
             self.ui.DownloadButton.setEnabled(False)
-    
+        #elif(self.chkCNES and self.chkBPA and self.chkSIHD2 and self.chkSISAIH01):
+            
+
+    def log_status(self, msg):
+        self.ui.text_STATUS.appendPlainText(msg)
+
     def verifica_checked(self):
+        self.chkCNES = self.ui.checkCNES.isChecked()
         self.chkBPA = self.ui.checkBPA.isChecked()
         self.chkSISAIH01 = self.ui.checkSISAIH01.isChecked()
         self.chkSIHD2 = self.ui.checkSIHD2.isChecked()
-
+        
+        if(self.chkCNES):
+            self.download_CNES()
         if(self.chkBPA):
             self.download_BPA()
         if(self.chkSISAIH01):
@@ -71,27 +85,51 @@ class Download_FTP:
         self.size_file = (self.ftp.size(filename) / 1e+6)
 
         self.verifica_dir(local_dir)
-        # Download do arquivo
         
+        # Download do arquivo
         self.ui.text_STATUS.appendPlainText(
             '--> Baixando Arquivo {} —> {} MB'.format(filename, self.size_file))
         self.local_filename = os.path.join(r"{}".format(local_dir), filename)
         self.lf = open(self.local_filename, "wb")
         self.ftp.retrbinary("RETR " + filename, self.lf.write, 8*1024)
         self.lf.close()
-        
+    
+    def download_CNES(self):
+            self.user_os = getpass.getuser()
+            self.compt = self.ui.input_CMPT_CNES.text()
+            self.compt = self.compt.split('/')
+
+            self.ftp_host = "ftp.datasus.gov.br"
+            self.local_dir = '/Users/{}/Downloads/Arquivos_Atualizacao/CNES'.format(
+                self.user_os)
+
+            self.files = {
+                'Fornec': ['/cnes/', 'FORNEC_{}.ZIP'.format(self.compt[1]+self.compt[0])],
+                'Import': ['/cnes/', 'IMPORT_{}.ZIP'.format(self.compt[1]+self.compt[0])],
+                'TercBR': ['/cnes/', 'TERCEIROSBRASIL24_{}.ZIP'.format(self.compt[1]+self.compt[0])],
+            }
+
+            for key in self.files.keys():
+                self.download_file(self.ftp_host, "", "",
+                self.files[key][0], self.files[key][1], self.local_dir)
+
+                self.msg = '--> {} Baixado com Sucesso!'.format(self.files[key][1])
+                self.log_status(self.msg)
+
+
+
     def download_BPA(self):
         self.user_os = getpass.getuser()
         self.filename = self.ui.input_BPA.text()
         self.ftp_host = "arpoador.datasus.gov.br"
         self.remote_dir = "/siasus/sia/"
         self.local_dir = '/Users/{}/Downloads/Arquivos_Atualizacao/BPA-SIA'.format(self.user_os)
+        self.msg = '--> {} Baixado com Sucesso!\n'.format(self.filename)
 
         self.download_file(self.ftp_host, "", "", self.remote_dir,
                       self.filename, self.local_dir)
 
-        self.ui.text_STATUS.appendPlainText(
-            '--> {} Baixado com Sucesso!\n'.format(self.filename))
+        self.log_status(self.msg)
 
     def download_SISAIH01(self):
         self.user_os = getpass.getuser()
@@ -101,12 +139,11 @@ class Download_FTP:
         self.ftp_host = "ftp2.datasus.gov.br"
         self.remote_dir = "/public/sistemas/dsweb/SIHD/Programas/"
         self.local_dir = '/Users/{}/Downloads/Arquivos_Atualizacao/SISAIH01'.format(self.user_os)
-
+        self.msg = '--> {} Baixado com Sucesso!\n'.format(self.filename)
         self.download_file(self.ftp_host, "", "", self.remote_dir,
                       self.filename, self.local_dir)
 
-        self.ui.text_STATUS.appendPlainText(
-            '--> {} Baixado com Sucesso!\n'.format(self.filename))
+        self.log_status(self.msg)
 
     def download_SIHD2(self):
         self.user_os = getpass.getuser()
@@ -136,8 +173,9 @@ class Download_FTP:
             else:
                 self.download_file(self.ftp_host_1, "", "",
                                    self.files[key][0], self.files[key][1], self.local_dir)
-            self.ui.text_STATUS.appendPlainText(
-                '--> {} Baixado com Sucesso!'.format(self.files[key][1]))
+
+            self.msg = '--> {} Baixado com Sucesso!'.format(self.files[key][1])
+            self.log_status(self.msg)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
