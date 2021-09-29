d97b026a22464f8b78e1029206e5a66e10dff0aa
Willamy Domingos <willamy.wlp@gmail.com>
2021-09-29 00:36:37 -0300

    Verificando Checkboxes

--- main.py
+++ main.py
@@ -11,11 +11,32 @@ class Download_FTP:
         self.ui = Ui_Sys_Datasus()
         self.ui.setupUi(self.main_win)
 
+        self.ui.checkBPA.clicked.connect(self.checked)
+        self.ui.checkSISAIH01.clicked.connect(self.checked)
+        self.ui.checkSIHD2.clicked.connect(self.checked)
+
     def show(self):
         self.main_win.show()
 
-    
+    def checked(self):
+        if(self.ui.checkBPA.isChecked()):
+            self.ui.input_BPA.setEnabled(True)
+        else:
+            self.ui.input_BPA.setEnabled(False)
 
+        if(self.ui.checkSISAIH01.isChecked()):
+            self.ui.input_SISAIH01.setEnabled(True)
+        else:
+            self.ui.input_SISAIH01.setEnabled(False)
+        
+        if(self.ui.checkSIHD2.isChecked()):
+            self.ui.input_CMPT.setEnabled(True)
+            self.ui.input_VERSAO.setEnabled(True)
+        else:
+            self.ui.input_CMPT.setEnabled(False)
+            self.ui.input_VERSAO.setEnabled(False)
+        
+    
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main_win = Download_FTP()
